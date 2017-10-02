20c691397aafff6c8a8c381eb595254b03a1b440
Rebecca McCann-Young <Rebecca.McCann-Young@onsolve.com>
2017-10-01 20:22:12 -0500

    Changed around some objects, added optional date tag for back/forward dating entries.

--- main.go
+++ main.go
@@ -6,13 +6,14 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"runtime"
+	"strconv"
+	"strings"
 	"time"
 
-	"github.com/andygrunwald/go-jira"
+	jira "github.com/andygrunwald/go-jira"
 )
 
 var (
@@ -27,15 +28,23 @@ type AccountInfo struct {
 	jiraUser jira.User
 }
 
+type InputData struct {
+	issueKey     string
+	time2Log     string
+	logTimeStamp string
+	comment      string
+}
+
 func main() {
 
 	var showVer bool
-	var issueKey, timeToAdd, timeComment string
+	var issueKey, timeToAdd, timeComment, datestamp string
 
 	flag.BoolVar(&showVer, "v", false, "Shows version")
 	flag.StringVar(&issueKey, "ticket", "", "The ticket to add time to - please use the format of PROJ-ticketnumber")
 	flag.StringVar(&timeToAdd, "time", "", "Please user Jira's time format of 1h 30m to log time")
 	flag.StringVar(&timeComment, "worklog", "", "The worklog comment")
+	flag.StringVar(&datestamp, "date", "", "The date the worklog should be dated to - omit for today - expected format MM/DD/YYYY")
 	flag.Parse()
 
 	if showVer {
@@ -50,6 +59,17 @@ func main() {
 		os.Exit(0)
 	}
 
+	if datestamp == "" {
+		datestamp = time.Now().Format("01/02/2006")
+	}
+
+	workLog := InputData{
+		issueKey:     issueKey,
+		comment:      timeComment,
+		time2Log:     timeToAdd,
+		logTimeStamp: datestamp,
+	}
+
 	if os.Getenv("jira_url") == "" || os.Getenv("jira_username") == "" || os.Getenv("jira_password") == "" {
 		fmt.Println("This program requires environment variables be set for jira_username, jira_password with your account information, and jira_url for the base url for the Jira instance you want to log your time against")
 		os.Exit(0)
@@ -59,13 +79,8 @@ func main() {
 
 	checkAccess(issueKey, account)
 
-	worklogentry := jira.WorklogRecord{
-		IssueID:   issueKey,
-		Comment:   timeComment,
-		TimeSpent: timeToAdd,
-	}
 	if err == nil && account != nil {
-		addWorklog(account, worklogentry)
+		addWorklog(account, workLog)
 	} else {
 		fmt.Println("the following err occured ", err)
 	}
@@ -108,22 +123,34 @@ func getAccountInfo() (*AccountInfo, error) {
 
 }
 
-func addWorklog(account *AccountInfo, entry jira.WorklogRecord) error {
+func addWorklog(account *AccountInfo, entry InputData) error {
 
-	uri := fmt.Sprintf("%srest/api/2/issue/%s/worklog", account.jiraURL, entry.IssueID)
+	uri := fmt.Sprintf("%srest/api/2/issue/%s/worklog", account.jiraURL, entry.issueKey)
+	fmt.Println("Worklog time added for: ", entry.logTimeStamp)
+	date := strings.Split(entry.logTimeStamp, "/")
+	month, err := strconv.Atoi(date[0])
+	day, err := strconv.Atoi(date[1])
+	year, err := strconv.Atoi(date[2])
+
+	newTime := time.Date(year, time.Month(month), day, 12, 0, 0, 0, time.Local)
+
+	if err != nil {
+		fmt.Println("Could not parse time ", entry.logTimeStamp)
+		os.Exit(0)
+	}
 
 	worklogData := map[string]interface{}{
-		"comment":   entry.Comment,
-		"timeSpent": entry.TimeSpent,
-		"started":   time.Now().UTC().Format("2006-01-02T15:04:05.000-0700"),
+		"comment":   entry.comment,
+		"timeSpent": entry.time2Log,
+		"started":   newTime.UTC().Format("2006-01-02T15:04:05.000-0700"),
 	}
 
 	b, err := json.Marshal(worklogData)
 	jsonStr := string(b)
-
+	fmt.Println("json request string ", jsonStr)
 	resp, err := makeRequestWithContent("POST", uri, jsonStr, account)
 	if err != nil {
-		fmt.Println("An error occured", err)
+		//fmt.Println("An error occured", err)
 		return err
 	}
 
@@ -173,8 +200,8 @@ func makeRequest(req *http.Request, account *AccountInfo) (resp *http.Response, 
 
 	if resp.StatusCode != 201 {
 		fmt.Println("Return code from post request: ", resp.StatusCode)
-		b, _ := ioutil.ReadAll(resp.Body)
-		fmt.Println("Error Body: ", string(b))
+		//b, _ := ioutil.ReadAll(resp.Body)
+		//fmt.Println("Error Body: ", string(b))
 	}
 
 	return resp, nil
